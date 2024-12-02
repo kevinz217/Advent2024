676f80f60382832e5027a746c39ce131e5acadce
BT_4N2_11 <kevinz217@nycstudents.net>
2024-12-02 13:14:11 -0500

    Part 1 day one done

--- src/Day1.java
+++ src/Day1.java
@@ -1,16 +1,28 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Day1 {
     public static void main(String[] args) {
 
         ArrayList<String> fileData = getFileData("src/Day1Input.txt");
-        ArrayList<Number> list1 =
+        ArrayList<Integer> list1 = new ArrayList<Integer>();
+        ArrayList<Integer> list2 = new ArrayList<Integer>();
+        int total = 0;
         for (int i = 0; i < fileData.size(); i++) {
-
+            String[] line = (fileData.get(i)).split("   ");
+            list1.add(Integer.parseInt(line[0]));
+            list2.add(Integer.parseInt(line[1]));
         }
+        Collections.sort(list1);
+        Collections.sort(list2);
+        for (int i = 0; i < 1000; i++) {
+            total += Math.abs(list1.get(i) - list2.get(i));
+        }
+        System.out.println(total);
     }
 
     public static ArrayList<String> getFileData(String fileName) {
