467f7011b9af61abfee34731fb3a15b038587b56
unknown <BT_4N2_11@BKTECH.LOCAL>
2024-12-02 12:53:10 -0500

    setting up

---
+++ src/Day1.java
@@ -0,0 +1,32 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Day1 {
+    public static void main(String[] args) {
+
+        ArrayList<String> fileData = getFileData("src/Day1Input.txt");
+        ArrayList<Number> list1 =
+        for (int i = 0; i < fileData.size(); i++) {
+
+        }
+    }
+
+    public static ArrayList<String> getFileData(String fileName) {
+        ArrayList<String> fileData = new ArrayList<String>();
+        try {
+            File f = new File(fileName);
+            Scanner s = new Scanner(f);
+            while (s.hasNextLine()) {
+                String line = s.nextLine();
+                if (!line.equals(""))
+                    fileData.add(line);
+            }
+            return fileData;
+        }
+        catch (FileNotFoundException e) {
+            return fileData;
+        }
+    }
+}
