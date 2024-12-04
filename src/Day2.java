469151f1df051e4e4c292c4f27c94a2e75b94217
BT_4N2_11 <sample@bths>
2024-12-04 13:17:16 -0500

    day 2 part 1 finished started part 2

--- src/Day2.java
+++ src/Day2.java
@@ -8,7 +8,7 @@ public class Day2 {
         ArrayList<String> fileData = getFileData("src/Day2Input.txt");
 
         System.out.println("Part one answer : " + partOne(fileData));
-        //System.out.println("Part two answer : " + partTwo());
+        System.out.println("Part two answer : " + partTwo(fileData));
     }
 
     public static ArrayList<String> getFileData(String fileName) {
@@ -29,6 +29,7 @@ public class Day2 {
 
     public static int partOne(ArrayList<String> fileData) {
         int ans = 0;
+
         for (int i = 0; i < fileData.size(); i++) {
             String[] report = (fileData.get(i)).split(" ");
             boolean safe = true;
@@ -38,29 +39,97 @@ public class Day2 {
                 increasing = true;
             }
 
-            for (int idx = 1; idx < report.length- 1; idx++) {
-                int abs = Math.abs(Integer.parseInt((report[idx])) - Integer.parseInt((report[idx + 1])));
-                if (abs > 3 || abs < 1 ) {
-                    System.out.println("This is not safe since it has a large difference: " + Arrays.toString(report));
-                    safe = false;
-                    break;
-                }
-                if (increasing) {
-                    if (Integer.parseInt((report[idx])) > Integer.parseInt(report[idx + 1])) {
-                        System.out.println("This is not safe since it is not consistent: " + Arrays.toString(report));
+            int firstAbs = Math.abs(Integer.parseInt((report[0])) - Integer.parseInt((report[1])));
+            if (firstAbs > 3 || firstAbs < 1 ) {
+                System.out.println("This is not safe since it has a large difference: " + Arrays.toString(report));
+                safe = false;
+            } else {
+                for (int idx = 1; idx < report.length - 1; idx++) {
+                    int abs = Math.abs(Integer.parseInt((report[idx])) - Integer.parseInt((report[idx + 1])));
+                    if (abs > 3 || abs < 1) {
+                        System.out.println("This is not safe since it has a large difference: " + Arrays.toString(report));
                         safe = false;
                         break;
                     }
-                } else {
-                    if (Integer.parseInt((report[idx])) < Integer.parseInt(report[idx + 1])) {
-                        System.out.println("This is not safe it is not consistent: " + Arrays.toString(report));
-                        safe = false;
-                        break;
+
+                    if (increasing) {
+                        if (Integer.parseInt((report[idx])) > Integer.parseInt(report[idx + 1])) {
+                            System.out.println("This is not safe since it is not consistent: " + Arrays.toString(report));
+                            safe = false;
+                            break;
+                        }
+                    } else {
+                        if (Integer.parseInt((report[idx])) < Integer.parseInt(report[idx + 1])) {
+                            System.out.println("This is not safe it is not consistent: " + Arrays.toString(report));
+                            safe = false;
+                            break;
+                        }
                     }
                 }
             }
+
             if (safe) {
                 ans++;
+            }
+        }
+        return ans;
+    }
+
+    public static int partTwo(ArrayList<String> fileData) {
+        int ans = 0;
+
+        for (int i = 0; i < fileData.size(); i++) {
+            //gets one line
+            String[] report = (fileData.get(i)).split(" ");
+            ArrayList<String> reportVer1 = (ArrayList<String>) Arrays.asList(report);
+            ArrayList<String> reportVer2 = (ArrayList<String>) Arrays.asList(report);
+            boolean removedVal = false;
+            boolean safe1 = true;
+            boolean safe2 = true;
+            boolean increasing = false;
+
+            if (Integer.parseInt((report[0])) < Integer.parseInt(report[1])) {
+                increasing = true;
+            }
+
+            int firstAbs = Math.abs(Integer.parseInt((report[0])) - Integer.parseInt((report[1])));
+            if (firstAbs > 3 || firstAbs < 1 ) {
+                // difference is either too big or too small
+                removedVal = true;
+                reportVer1.remove(0);
+                reportVer2.remove(1);
+            } else {
+                // checks index of the report
+                for (int idx = 1; idx < report.length - 1; idx++) {
+                    int abs = Math.abs(Integer.parseInt((report[idx])) - Integer.parseInt((report[idx + 1])));
+                    if (abs > 3 || abs < 1) {
+                        // difference is either too big or too small
+                        safe1 = false;
+                        break;
+                    }
+
+                    if (increasing) {
+                        if (Integer.parseInt((report[idx])) > Integer.parseInt(report[idx + 1])) {
+                            // it suddenly decreases
+                            safe1 = false;
+                            break;
+                        }
+                    } else {
+                        if (Integer.parseInt((report[idx])) < Integer.parseInt(report[idx + 1])) {
+                            // it suddenly increases
+                            safe1 = false;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            if (safe1) {
+                ans++;
+            } else {
+                if (safe2) {
+                    ans++;
+                }
             }
         }
         return ans;
